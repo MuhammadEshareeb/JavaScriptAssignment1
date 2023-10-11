34096a5f3b031d1ced171db8464f5303fddcae65
MuhammadEshareeb <141601484+MuhammadEshareeb@users.noreply.github.com>
2023-10-12 01:24:04 +0500

    Add files via upload

---
+++ JavaScriptAssignment1/app.js
@@ -0,0 +1,139 @@
+// START
+// Chapter 1 (Alerts)
+// 1. Alert these following (individually): 
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// // V. Password
+// Answer
+alert("Muhammad\nEshareeb\neshareeb@gmail.comn\n0313-0123456\n#eshareeb#(0123)")
+// 2. Correct this statement: alert"You're learning JavaScript!";
+// Answer
+alert("You're learning JavaScript!");
+// 3. Code an alert statement displaying any message you like.
+// Answer
+alert("1) A teacher is a great source of knowledge, prosperity, and enlightenment from which anybody can be benefited.\n 2) Teachers are the gifts of god who make our career and guide us towards success.\n 3) Teachers make their students academically strong and they always encourage their students to do better in their life")
+// Chapter 2 (Variables for string)
+// 1. Declare any variable in the camelCase format.
+// Answer
+var fullName;
+fullName = "Muhammad Eshareeb";
+// 2. Declare a variable of your choice without defining it. Then, in a 
+// second statement, assign it a string of your choice.
+// Answer
+var  myVariable;
+myVariable = "This is a string";
+// 3. Declare the variable teamName and Alert your Team name.
+// Answer
+var teamName;
+teamName = "Muhammad Eshareeb Rajput";
+// 4. This statement has already been coded. var bestMan = "Charlie";
+// Assign the variable a new string.
+// Answer
+var bestMan = "Charlie";
+bestMan = "Pakistan";
+//  Chapter 3 (Variables for numbers)
+// 1. Declare a variable “caseQty”
+// Answer
+var caseQty;
+// 2. Assign to the variable caseQty, which has already been declared,
+// the value 144.
+// Answer
+caseQty = 144;
+// 3. Rewrite this statement so the variable can be used in a math
+// operation. var num = "9";
+// Answer
+var num1 = "9";
+var num2 = "9";
+console.log(num1 = num2)
+// 4. In one statement declare a variable. In a second statement assign
+// it the sum of 2 numbers.
+// Answer
+var num1 = "2";
+var num2 = "num1+2";
+// 5. What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+// Answer
+var merchTotal = 10;
+var shippingCharge = 100;
+var orderTotal = merchTotal + shippingCharge;
+// 6. In the first statement declare a variable and assign it a number. In
+// the second statement, change the value of the variable by adding
+// it together with a number.
+// Answer
+var num = "5";
+num= 10 + 2 ;
+console.log(num); 
+// Chapter 4 (Variable names Legal and Illegal)
+// 1. Correct this statement.
+// var product cost = 3.45;
+// Answer
+var productCost = "3.45";
+// 2. Rewrite this using camelCase.
+// var Nameofband;
+// Answer
+var nameOfBrand;
+// 3. In a single statement declare a legally-named variable and assign a
+// number to it.
+// Answer
+var firstName = "10";
+// 4. Declare a variable that is a combination of your first and last
+// names. Use camelCase.
+// Answer
+var myFirstNameLastName= "Muhammad Eshareeb";
+// 5. List the legal and Illegal Variables.
+// Answer
+// legal
+var fullName;
+var full_name;
+var productCost = "3.45";
+// Illegal 
+var fullname;
+var productcost = 3.45;
+// Chapter 5 (Math Expression I)
+// 1. What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+// Answer
+var dividend = "10";
+var divisor = "3";
+var remainder = dividend % divisor;
+console.log(remainder);
+// 2. What is the value of num?
+// var num = 20 % 6;
+// Answer
+var num = 20 % 6 ;
+console.log(num);
+// 3. In a single statement, declare the variable largeNum and assign it
+// the result of 1,000 multiplied by 2,000.
+// Answer
+var largeNum = 1000 * 2000 ;
+console.log(largeNum);
+// 4. Assign to a variable the value represented by one variable
+// subtracted from the value represented by another variable
+// Answer
+var variable1 = "4";
+var variable2 = "6";
+var result = variable1 - variable2;
+console.log(result);
+// 5. Assign to a variable the remainder when one number is divided by
+// another. The variable hasn't been declared beforehand. Make up
+// the variable name.
+// Answer
+var numm = 30 % 10 ;
+console.log(numm);
+var num = 20 % 6 ;
+console.log(num);
+// 6. Code an alert that displays the result of a multiplication on 2
+// numbers.
+// Answer
+var multipliA = "0.5" ; 
+var multipliB = "0.88888" ; 
+var answer = multipliA * multipliB
+console.log( answer);
+alert("answer")
+
+// <<< THE END >>>
